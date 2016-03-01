de602b789fd4dd77361f818c4dc186f0f7abe5a8
ram1991 <makievgen@gmail.com>
2016-03-01 17:26:10 +0200

    01.03.2016 rm

---
+++ app/src/main/java/com/example/ram1991/creativewebmedianewsfeed/interactors/models/Constants.java
@@ -0,0 +1,15 @@
+package com.example.ram1991.creativewebmedianewsfeed.interactors.models;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String HEAD_LINE = "headLine";
+    public static final String DATE_LINE = "dateLine";
+    public static final String WEB_URL = "WebURL";
+    public static final String URL = "URL";
+    public static final String BASE_URL = "http://timesofindia.indiatimes.com/";
+
+
+}
