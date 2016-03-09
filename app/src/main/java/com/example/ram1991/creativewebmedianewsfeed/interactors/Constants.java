a40b8cc6a4b418e5cd2137c6e94408f25e2812eb
ram1991 <makievgen@gmail.com>
2016-03-09 16:31:58 +0200

    09.03.2016 rm

--- app/src/main/java/com/example/ram1991/creativewebmedianewsfeed/interactors/Constants.java
+++ app/src/main/java/com/example/ram1991/creativewebmedianewsfeed/interactors/Constants.java
@@ -5,8 +5,6 @@ public final class Constants {
     private Constants() {
     }
 
-    public static final String HEAD_LINE = "headLine";
-    public static final String DATE_LINE = "dateLine";
     public static final String WEB_URL = "WebURL";
     public static final String BASE_URL = "http://timesofindia.indiatimes.com/";
 
